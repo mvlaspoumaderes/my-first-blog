11e7a5a411e93accab72e138c83d6b98fd40c3ec
mvlaspoumaderes <mv.laspoumaderes@gmail.com>
2018-11-30 16:46:33 +0000

    Login/Logout layour

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,6 @@ from .forms import PostForm
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from django.contrib import auth
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -58,34 +57,3 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
-
-def login(request):
-    if request.user.is_authenticated():
-        return redirect('admin_page')
- 
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = auth.authenticate(username=username, password=password)
- 
-        if user is not None:
-            # correct username and password login the user
-            auth.login(request, user)
-            return redirect('admin_page')
- 
-        else:
-            messages.error(request, 'Error wrong username/password')
- 
-    return render(request, 'blog/login.html')
- 
- 
-def logout(request):
-    auth.logout(request)
-    return render(request,'blog/logout.html')
- 
- 
-def admin_page(request):
-    if not request.user.is_authenticated():
-        return redirect('blog_login')
- 
-    return render(request, 'blog/admin_page.html')
